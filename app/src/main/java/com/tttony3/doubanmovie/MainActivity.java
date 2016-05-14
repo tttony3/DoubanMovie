dc99097885800636525231915f81edc0a448ce42
tttony3 <tonylily11@gmail.com>
2016-05-14 13:30:47 +0800

    1

--- app/src/main/java/com/tttony3/doubanmovie/MainActivity.java
+++ app/src/main/java/com/tttony3/doubanmovie/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //test
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
